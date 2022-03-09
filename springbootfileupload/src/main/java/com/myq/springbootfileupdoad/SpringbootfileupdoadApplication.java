c5e64e949b84ba399e3ed98225817dfccbccde47
MSYWO <96286647+MYY1SEN@users.noreply.github.com>
2022-03-09 19:54:37 +0800

    1

---
+++ springbootfileupload/src/main/java/com/myq/springbootfileupdoad/SpringbootfileupdoadApplication.java
@@ -0,0 +1,13 @@
+package com.myq.springbootfileupdoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfileupdoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootfileupdoadApplication.class, args);
+    }
+
+}
